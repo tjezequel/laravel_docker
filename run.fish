1e08eae1477a0fefc1080669eaf0c3196dc60438
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2018-11-23 13:59:39 +0100

    Added Env variables

--- run.fish
+++ run.fish
@@ -1,5 +1,6 @@
 #!/usr/local/bin/fish
 
 docker-compose up -d
+docker-compose exec php export APP_KEY=$(php artisan key:generate)
 docker-compose exec php composer install
 docker-compose exec php php artisan migrate
